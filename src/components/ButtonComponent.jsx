16c30a6bb7f0b88717517f5c51493a2f5bc8b176
Rahul garg <rgarg3577@gmail.com>
2024-07-20 13:07:45 +0530

    uodated buttonComponent

--- src/components/ButtonComponent.jsx
+++ src/components/ButtonComponent.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const ButtonComponent = ({ value, color, handleClick }) => {
   return (
     <button
-      className={`w-20 h-20 bg-${color} text-white text-3xl hover:border`}
+      className={`w-20 h-20 ${
+        value === "AC" ? `bg-blue-700` : `bg-black`
+      } text-white text-3xl hover:border`}
       onClick={handleClick}
       value={value}
     >
